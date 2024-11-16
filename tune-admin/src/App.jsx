65a8baed48b6802bb0ec82afbcee234586ace98a
Shabes7171 <155089733+Shabes7171@users.noreply.github.com>
2024-11-16 20:36:21 +0530

    Update App.jsx

--- tune-admin/src/App.jsx
+++ tune-admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from "./pages/ListAlbum";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 
-export const url='https://tune-flow-full-stack.onrender.com'
+export const url='http://localhost:5000'
 
 function App() {
   return (
